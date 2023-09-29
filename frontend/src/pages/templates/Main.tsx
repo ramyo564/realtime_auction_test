26cc7dd9484e5d70cf0725595c9bdcef5e0b57ee
ramyo564 <yohan032yohan@gmail.com>
2023-09-30 05:59:09 +0900

    feat: Axios 설치 및 jwt 인터셉터 설정

--- frontend/src/pages/templates/Main.tsx
+++ frontend/src/pages/templates/Main.tsx
@@ -1,6 +1,17 @@
 import { Box, Typography } from "@mui/material";
 
+
+import axios from "axios";
+
 const Main = () => {
+    axios.get("http://127.0.0.1:8000/products/all-products")
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
     return <Box sx={{ flexGrow: 1, }}>
 
         {[...Array(50)].map((_, i) => (
